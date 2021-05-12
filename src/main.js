d6272a151f95d26291ff51fceb5b43b88d37794f
Eder Lima <eder@ederlima.com.br>
2021-05-12 17:03:02 -0400

    testes basicos do servico em main.js

--- src/main.js
+++ src/main.js
@@ -3,13 +3,13 @@ export const search = (query, type) =>
   fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`, {})
     .then((response) => response.json())
 
-export const searchArtists = (artist) =>
-  search(artist, 'artist')
-export const searchAlbums = () =>
-  {}
+export const searchArtists = (query) =>
+  search(query, 'artist')
+export const searchAlbums = (query) =>
+  search(query, 'album')
 
-export const searchTracks = () =>
-  {}
+export const searchTracks = (query) =>
+  search(query, 'track')
 
-export const searchPlaylist = () =>
-  {}
+export const searchPlaylist = (query) =>
+  search(query, 'playlist')
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -14,6 +14,7 @@ describe('Main', () => {
     let promise
 
     beforeEach(() => {
+        //stub substitui o global fetch
         fetchedStub = sinon.stub(global, 'fetch')
         promise = fetchedStub.returnsPromise() // retorna a promise
     })
@@ -85,10 +86,50 @@ describe('Main', () => {
             expect(fetchedStub).to.have.been.calledOnce            
         })
         it('Verificando a url da chamada', () => {
-            const albums = searchArtists('Incubus')
+            const artist = searchArtists('Incubus')
             expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Incubus&type=artist')
-            const album2 = searchArtists('Muse')
+            const artist2 = searchArtists('Muse')
             expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Muse&type=artist')
+        })
+    })
+    describe('Search Albums', () => {
+        it('Verificar se o fetch foi chamado', () => {
+            const album = searchAlbums('Album X')
+            expect(fetchedStub).to.have.been.calledOnce
+        })
+        it('Verificar a url da chamada', () => {
+            const albumUrl = searchAlbums('AlbumY')
+            expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=AlbumY&type=album')
+            const albumUrl2 = searchAlbums('AlbumZ')
+            expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=AlbumZ&type=album')
+        })
+    })
+    describe('Search Tracks', () => {
+        it('Verificar se o fetch foi chamado', () => {
+            const trackName = searchTracks('TrackOne')
+            expect(fetchedStub).to.have.been.calledOnce
+        } )
+        it('Verificar se a url chamada é a correta', () => {
+            const trackSearchUrl = searchTracks('TrackOne')
+            expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=TrackOne&type=track')
+        })
+        it('Verificar se a url está devidamente parametrizada', () => {
+            const trackSearchUrl2 = searchTracks('TrackTwo')
+            expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=TrackTwo&type=track')
+        })
+    })
+    describe('Search Playlists', () => {
+        it('Verificar se o fetch foi chamado', () => {
+            const playlist = searchPlaylist('PlaylistOne')
+            expect(fetchedStub).to.have.been.calledOnce
+        })
+        it('Verificar se o fetch chama a do spotify', () => {
+            const playlistUrl = searchPlaylist('PlaylistOne')
+            expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=PlaylistOne&type=playlist')
+        })
+        it('Verificar se a searchPlaylist está parametrizada', () => {
+            const playlistUrlParam = searchPlaylist('PlaylistTwo')
+            expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=PlaylistTwo&type=playlist')
         })
     })
 });
