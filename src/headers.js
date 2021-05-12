b7f1c25ca46faf5502a09c61bb7eda6436a857f4
Eder Lima <eder@ederlima.com.br>
2021-05-12 17:43:36 -0400

    Uso de variáveis de ambiente para esconder o token

--- src/headers.js
+++ src/headers.js
@@ -1,5 +1,8 @@
+//requerendo o dotenv para poder criar e não somente ler variáveis de ambiente no backend
+require('dotenv').config({ path: '../.env' })
+
 export default {
     Accept: 'application/json',
-    Authorization: 'Bearer BQAWKZ1LKfjCXG8wJuinzFhoIEZzbCgGHnNkKyjEU748ErCILjD0kjEaZLJukDOSshuvJs4RUd0qsbvdo6eKoBuEZF51Hie_vBPC3REYgTirq13MHfQeyN9BPtKULybh2-luoNuU2kOlBCAnqq873d8QmRPX_KKVhNTTqImHCPp8Yt--BLaWlCcbDsDkx1qNVtWqvMjNE6-d_61BsyndTdSmm2ucaRKIzqUcu5xZfd3R2YKd-Pf5it3xOvicZ5hl_uofUS1iwJikIwsXHHCyP6-o4Q',
+    Authorization: process.env.API_TOKEN,
     'Content-Type': 'application/json'
 }
